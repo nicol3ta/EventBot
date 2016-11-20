2c427f727a262429950826944f5413717364e08c
Nicoleta Mihali <nikk_mihali@yahoo.de>
2016-11-20 11:15:56 +0100

    Add file
    Added an index.html

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var builder = require('botbuilder');
 var restify = require('restify');
-var _meetup = require('meetup-api')({ 
-     key: '765a70243d32a7f6562451810203074' 
- }); 
+var assert = require('assert'); 
+
+
+var authorization = 'https://secure.meetup.com/oauth2/authorize';
+var accessTokens = 'https://secure.meetup.com/oauth2/access'; 
+
+
+// OAuth2 variables for authenticating with the Meetup API
+
+// URL for requesting authorisation
+var redirectUri = 'https://secure.meetup.com/oauth2/authorize?client_id=ofgbuddccntus3nqm7mkrlrfi3&response_type=code&redirect_uri=http://localhost:3978/api/oauthcallback';
+// URL for requesting access token
+var address = 'https://secure.meetup.com/oauth2/access';
+
+var oauth_token;
+var refresh_token;
+
+// City where the user attends meetups
+var city;
+
 
 //=========================================================
 // Bot Setup
@@ -10,11 +27,20 @@ var _meetup = require('meetup-api')({
 
 // Setup Restify Server
 var server = restify.createServer();
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
+ 
   
 // Create chat bot
+// appId & appPassword settings are generally required and will be generated when registering your bot in the developer portal
+// Retrieve appId & appPassword settings from environment variables. This sets up the bot to support storing these values in a config file when deployed to a hosting service like Microsoft Azure
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -23,17 +49,60 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
+// Create oauth callback endpoint
+server.get("/api/oauthcallback", function (req, res, next) {  
+
+code = req.query.code;
+res.send(200, "You can now return to the Meetup Guru bot ;)" ); 
+
+
+var client = restify.createStringClient({
+    url : 'https://secure.meetup.com'
+});
+
+var body = {
+    client_id: 'ofgbuddccntus3nqm7mkrlrfi3',
+    client_secret: 'i9m1hgmfif5nhvtkr0vurkdaq3', 
+    grant_type: 'authorization_code', 
+    redirect_uri: 'http://localhost:3978/api/oauthcallback', 
+    code: code
+};
+
+client.post('/oauth2/access', body, function(err, req, res, data) {
+
+    if (err) {
+        console.log('ERROR! :(');
+        console.log(err);
+    } else {
+        console.log('YAY!');
+        console.log(data);
+        var result = JSON.parse(data);
+        console.log(result.access_token);
+
+        oauth_token = result.access_token;
+    
+    refresh_token = result.refresh_token;
+    console.log("I set the token to: " + oauth_token);
+    }
+
+
+
+});
+
+}); 
 
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our  Bot.
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Bot.
 var model = 'https://api.projectoxford.ai/luis/v1/application?id=8b82365c-bc74-4574-aed1-2107cffa24cb&subscription-key=245e66e437d6484d9bb1a0c6f706fb89';
 
 var recognizer = new builder.LuisRecognizer(model);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+
+// When the framework receives a message from the user it will be routed to this root ‘/’ dialog for processing
 bot.dialog('/', intents);
 
 
@@ -41,16 +110,27 @@ bot.dialog('/', intents);
  bot.use(builder.Middleware.firstRun({ version: 1.0, dialogId: '*:/firstRun' })); 
  bot.dialog('/firstRun', [ 
      function (session) { 
-         builder.Prompts.text(session, "Hello... What's your name?"); 
+         if (!session.userData.city) {
+            builder.Prompts.text(session, "Hey there, in which city do you need informations about Meetups?" ); 
+         }
+         else{ 
+             builder.Prompts.text(session, "Hey there!" ); 
+            }
      }, 
      function (session, results) { 
          // We'll save the users name and send them an initial greeting. All  
          // future messages from the user will be routed to the root dialog. 
-         session.userData.name = results.response; 
-         session.endDialog("Hi %s, you can ask ask me anything regarding meetups.", session.userData.name);  
-     } 
+         session.userData.city = results.response; 
+         city= session.userData.city;
+         session.send("Nice, %s is a cool city ;)", session.userData.city); 
+         if(!oauth_token){
+            session.endDialog("You can ask me anything regarding meetups. \n But first please sign in: %s", redirectUri); 
+         }
+         else{
+             session.endDialog("You can ask me anything regarding meetups."); 
+         }
+     }
  ]); 
-
 
 
 
@@ -76,36 +156,86 @@ intents.matches('getDate', [
     }
 ]);
 
-intents.matches('getLocation', builder.DialogAction.send('Searching for location'));
+intents.matches('getLocation', [
+    function (session, args, next) {
+        var eventName = builder.EntityRecognizer.findEntity(args.entities, 'EventName');
+        if (!eventName) {
+            builder.Prompts.text(session, "What's the meetup called?");
+        } else {
+            next({ response: eventName.entity });
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            // Search date for this event 
+            session.send("Searching for the location of the next " + results.response + " meetup");
+            getMeetupLocation(results.response, function(d){ if(d.toString()) session.send(d.toString()); else session.send("I'm sorry, I could not find any event called " + results.response + ".")  });
+                      
+        } else {
+            session.send("Ok");
+        }
+    }
+]);
+
 intents.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only look for meetup events."));
 
 
 
-//get the date of a meetup 
+
+//Get the date of a meetup 
 function getMeetupDate(name, callback){
 
-var meetupDate;
 
-_meetup.getOpenEvents({
-    'country' : 'de',
-    'city': 'München',
-    'text' : name,
-    'category' : '34'
-}, function(error, event) {
-    if (error) {
-        callback(d);
-    } else {
-        var d = "";
-        for (i = 0; i < event.results.length; i++) {
+var client = restify.createStringClient({
+    url : 'https://api.meetup.com'
+});
 
-        if(event.results[i].group.name.toLowerCase().includes(name.toLowerCase())) {
-             d = new Date(event.results[i].time); // Set the date to the epoch             
+var d = "";
+
+client.get('/2/open_events/?access_token='+oauth_token+'&sign=true&photo-host=public&country=de&category=34&text='+name+'&city='+city+'&page=20', function(err, req, res, obj) {
+    assert.ifError(err);
+    console.log("hmm");
+    console.log("we got smth :" + obj);
+    var events = JSON.parse(obj);
+    
+        for (i = 0; i < events.results.length; i++) {
+
+        if(events.results[i].group.name.toLowerCase().includes(name.toLowerCase())) {
+             d = new Date(events.results[i].time); // Set the date to the epoch             
          }
             
         }
         callback(d);
+    
+});
+
 
 }
-})
+
+//get the location of a meetup 
+function getMeetupLocation(name, callback){
+
+
+var client = restify.createStringClient({
+    url : 'https://api.meetup.com'
+});
+
+var l = "";
+
+client.get('/2/open_events/?access_token='+oauth_token+'&sign=true&photo-host=public&country=de&category=34&text='+name+'&city='+city+'&page=20', function(err, req, res, obj) {
+    assert.ifError(err);
+    console.log("hmm");
+    console.log("we got smth :" + obj);
+    var events = JSON.parse(obj);
+    
+        for (i = 0; i < events.results.length; i++) {
+
+        if(events.results[i].group.name.toLowerCase().includes(name.toLowerCase())) {
+             l = events.results[i].venue.address_1; //location of the venue          
+         }           
+        }
+        callback(l);
+    
+});
 
 }
